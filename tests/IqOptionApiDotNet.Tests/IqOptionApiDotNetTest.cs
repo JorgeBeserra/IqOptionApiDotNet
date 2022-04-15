8a0317218e69f763397e8be4674ebb5104dbc932
Jorge Beserra <jorgebeserra@gmail.com>
2022-04-15 19:48:37 -0300

    Update WorkFlow

--- tests/IqOptionApiDotNet.Tests/IqOptionApiDotNetTest.cs
+++ tests/IqOptionApiDotNet.Tests/IqOptionApiDotNetTest.cs
@@ -8,11 +8,11 @@ namespace IqOptionApiDotNet.unit
         //public async Task ConnectAsyncTask_WithValidUserId_ConnectMustBeTrue() {
             
 
-        //    //arrange
-        //    var api = new IqOptionApi(Configuration.Email, Configuration.Password);
+        //arrange
+        //var api = new IqOptionApiDotNet(Configuration.Email, Configuration.Password);
 
-        //    //act
-        //    var result = await api.ConnectAsync();
+        //act
+        //var result = await api.ConnectAsync();
 
         //    //assert
         //    result.ShouldBeTrue();
